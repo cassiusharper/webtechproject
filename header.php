622455ec3f9bc9076ba00d0467c332b3207643db
cassiusharper <45438744+cassiusharper@users.noreply.github.com>
2018-12-03 13:40:46 -0600

    Create header.php

---
+++ header.php
@@ -0,0 +1,25 @@
+<html>
+
+<head><title>Cars</title></head>
+<style>
+a {
+	text-decoration:none; 
+	color:white; 
+	font-size:19pt;
+	padding:20px;
+}
+a:hover {
+    background-color: teal;
+	color: orange;
+} 
+</style>
+<body>
+
+<h1>Welcome to my Car Rental Service</h1>
+
+<a href="form1.php">Home</a>
+<a href="https://www.google.com/maps/place/Chicago+Metropolitan+Area/@41.6978453,-89.0568835,8z/data=!3m1!4b1!4m5!3m4!1s0x880e444c8c99e083:0xda7b840a9a579334!8m2!3d41.7435073!4d-88.0118473">Map of the Chicagoland Area</a>
+<a href="https://www.dmv.org/">DMV Website</a>
+<a href="https://www.google.com/maps/place/Illinois/@40.0971031,-91.267924,7z/data=!4m5!3m4!1s0x880b2d386f6e2619:0x7f15825064115956!8m2!3d40.6331249!4d-89.3985283">Map of Illinois</a> 
+<hr style = "color:teal; height:4" />
+</html>
